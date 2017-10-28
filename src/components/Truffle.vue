1b3fcf099b710152d43dfe17b8cb5c46e16ca5c2
Joe LeBlanc <contact@jlleblanc.com>
2017-10-28 16:42:07 -0500

    Finally rendering everything that doesn't have a hostId

--- src/components/Truffle.vue
+++ src/components/Truffle.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="truffle">
-    <div>{{truffle.timestamp}}</div>
-    <div>{{truffle.id}}</div>
+    <a :href="anchor"></a>
+    <div class="date">{{date()}}</div>
+    <dl>
+      <template class="truffle-field" v-for="(value, key) in truffle" v-if="key != 'id' && key != 'hostId' && key != 'timestamp'">
+        <dt>{{key}}</dt>
+        <dd>{{value}}</dd>
+      </template>
+    </dl>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Truffle',
-  props: [ 'truffle' ]
+  props: [ 'truffle' ],
+  data () {
+    return {
+      anchor: '#' + this.truffle.id,
+      date () {
+        var isoDate = new Date(this.truffle.timestamp)
+        return isoDate.toDateString()
+      }
+    }
+  }
 }
 </script>
