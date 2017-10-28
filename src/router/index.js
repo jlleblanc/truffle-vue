a5edcc4fcef3026f17c8c28db07adefec8e9634a
Joe LeBlanc <contact@jlleblanc.com>
2017-10-28 17:24:09 -0500

    Cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import TruffleList from '@/components/TruffleList'
-import SingleTruffle from '@/components/SingleTruffle'
+import Truffle from '@/components/Truffle'
 
 Vue.use(Router)
 
@@ -15,7 +15,7 @@ export default new Router({
     {
       path: '/:truffleId',
       name: 'Truffle',
-      component: SingleTruffle
+      component: Truffle
     }
   ]
 })
